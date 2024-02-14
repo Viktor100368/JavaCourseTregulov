758253453c1aee7e43bde78f4507e24b7114fad5
Viktor100368 <Vbur100368@gmail.com>
2024-02-14 20:34:37 +0400

    update file Day4_3

--- src/anna_task/day4/Day4_3.java
+++ src/anna_task/day4/Day4_3.java
@@ -3,12 +3,19 @@ package anna_task.day4;
 import java.util.function.Function;
 
 public class Day4_3  {
+
     public static void main(String[] args) {
         int a = -4;
         int b = 56;
-        Function<Integer,Boolean> func= x-> x>0;
-        System.out.println("a > 0 - "+func.apply(a));
-        System.out.println("b > 0 - "+func.apply(b));
+        int c = 0;
+        Function<Integer,String> func= x-> {
+            if(x > 0)return "положительное";
+            else if(x == 0)return "нуль";
+            else return "отрицательное";
+        };
+        System.out.println(func.apply(a));
+        System.out.println(func.apply(b));
+        System.out.println(func.apply(c));
     }
 
 }
